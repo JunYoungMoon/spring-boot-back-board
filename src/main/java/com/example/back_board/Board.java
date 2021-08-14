c587aec1a557d2554f3f913aab38bc393378aa0d
JunYoungMoon <JunYoungMoon>
2021-08-14 12:12:47 +0900

    init

---
+++ src/main/java/com/example/back_board/Board.java
@@ -0,0 +1,18 @@
+package com.example.back_board;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+public class Board {
+    @RequestMapping("api/board")
+    public HashMap<String, Object> return_json() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("id labore ex et quam laborum", "Eliseo@gardner.biz");
+        map.put("quo vero reiciendis velit similique earum", "Jayne_Kuhic@sydney.com");
+        map.put("odio adipisci rerum aut animi", "Nikita@garfield.biz");
+        return map;
+    }
+}
